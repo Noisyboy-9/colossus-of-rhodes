cd59c0bb978cfec7327a29003d1a1e6ddbce1963
sina shariati <sina.shariati@yahoo.com>
2023-01-19 21:07:52 +0330

    Glue up everything together

--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
 
+	"github.com/noisyboy-9/colossus/dispatcher"
+	"github.com/noisyboy-9/colossus/hardware"
+	"github.com/noisyboy-9/colossus/process"
+	"github.com/noisyboy-9/colossus/queue"
+	"github.com/noisyboy-9/colossus/schedulers"
+)
+
+func main() {
+	// setup hardware
+	cpuReadyForNewTaskSignaller := make(chan bool)
+	h := hardware.InitHardware(cpuReadyForNewTaskSignaller)
+
+	// 	setup ready queue
+	readyQueue := queue.NewReadyQueue()
+	for i := 0; i < queue.DegreeOfMultiprogramming; i++ {
+		p := process.NewProcess(int64(i), time.Now(), 500*time.Millisecond)
+		readyQueue.Add(p)
+	}
+
+	// setup dispatcher
+	dsp := dispatcher.NewSimpleDispatcher(h.Cpu)
+
+	// 	setup scheduler
+	scheduler := schedulers.NewScheduler(schedulers.ROUND_ROBIN, h)
+	done := make(chan bool)
+	go func() {
+		for {
+			switch {
+			case <-done:
+				fmt.Println("simulation finished")
+				break
+
+			case <-cpuReadyForNewTaskSignaller:
+				selectedProcess := scheduler.Schedule(readyQueue)
+				dsp.Dispatch(selectedProcess)
+			}
+		}
+	}()
+
+	// start simulation
+	cpuReadyForNewTaskSignaller <- true
+	time.Sleep(10 * 500 * time.Millisecond)
+	done <- true
+	fmt.Print("hope you have enjoyed")
 }
