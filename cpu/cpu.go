b200bde7da45a22625dcda45a6f173ca01262a8b
sina shariati <sina.shariati@yahoo.com>
2023-01-19 18:23:56 +0330

    Finish cpu preemption structure

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,17 +1,57 @@
 package cpu
 
 import (
+	"errors"
+	"time"
+
 	"github.com/noisyboy-9/colossus/process"
 )
 
 type CPU struct {
-	executing *process.Process
+	executing          *process.Process
+	interruptPin       chan bool
+	ticker             *time.Ticker
+	readyForNewProcess chan bool
 }
 
-func NewCPU() *CPU {
-	return &CPU{}
+func NewCPU(newProcessChannel chan bool) *CPU {
+	return &CPU{
+		interruptPin:       make(chan bool),
+		ticker:             time.NewTicker(1 * time.Millisecond),
+		readyForNewProcess: newProcessChannel,
+	}
 }
 
 func (c *CPU) StartExecution(p *process.Process) {
 	c.executing = p
+
+	go func() {
+		for {
+			select {
+			case <-c.interruptPin:
+				c.preemptionRoutine()
+				break
+			case <-c.ticker.C:
+				if c.executing.IsFinished() {
+					_ = c.Interrupt()
+				}
+				c.executing.ReduceDuration(1 * time.Millisecond)
+			}
+		}
+	}()
+}
+
+func (c *CPU) Interrupt() error {
+	if c.executing == nil {
+		return errors.New("no process is in execution")
+	}
+
+	c.interruptPin <- true
+	return nil
+}
+
+func (c *CPU) preemptionRoutine() {
+	c.ticker.Stop()
+	c.executing = nil
+	c.readyForNewProcess <- true
 }
